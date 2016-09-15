44b5e1b6b61ec2fd7ba187a1440ce5319d083b71
Kenrick Tse <kt1556@nyu.edu>
2016-09-15 16:10:20 -0400

    programmed player movement and following camera

---
+++ Assets/playerMovement.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += new Vector3(0, 0, 10f * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += new Vector3(0, 0, -10f * Time.deltaTime) ;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.eulerAngles += new Vector3(0, -60f * Time.deltaTime, 0);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.eulerAngles += new Vector3(0, 60f * Time.deltaTime, 0);
+        }
+
+        Camera.main.transform.position = transform.position + new Vector3(0, 8f, -5f);
+    }
+}
