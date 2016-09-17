1f16f71246382263c6c6b497532d5a28cecff9cb
Kenrick Tse <kt1556@nyu.edu>
2016-09-17 01:58:11 -0400

    game essentially finished. just a matter of making less stupid 3D models now.

---
+++ Assets/hint3.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class hint3 : MonoBehaviour {
+
+    public Text hint;
+    public GameObject player;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((transform.position - player.transform.position).magnitude < 8f)
+        {
+            hint.text = "You are at the cube.\n Head east until you reach the tower.";
+        }
+        else
+        {
+            hint.text = "";
+        }
+    }
+}
---
+++ Assets/hint4.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class hint4 : MonoBehaviour {
+    public Text hint;
+    public GameObject player;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((transform.position - player.transform.position).magnitude < 8f)
+        {
+            hint.text = "You are at the tower.\n Head north until you reach your long forgotten gold.";
+        }
+        else
+        {
+            hint.text = "";
+        }
+    }
+}
